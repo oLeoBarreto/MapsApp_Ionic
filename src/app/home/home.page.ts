24b6ec3362088c17b31c2ab8ad59bde190abc61b
unknown <leobcontato@gmail.com>
2022-04-04 22:14:04 -0300

    Mostrando o mapa do google na tela

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  @ViewChild("map") mapRef: ElementRef;
+
+  map: google.maps.Map;
+  center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
+
+  constructor() { }
+
+  initMap(): void {
+    this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: this.center,
+      zoom: 8
+    });
+  }
+
+  ngOnInit(): void {
+      this.initMap();
+  }
 
 }
