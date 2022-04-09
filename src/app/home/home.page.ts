f1dbac945336149c0897ddd2e40d9d51f6013d7b
unknown <=>
2022-04-08 22:19:35 -0300

    Pegando e mostrando no mapa localização atual

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Geolocation, Position } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -10,19 +11,39 @@ export class HomePage implements OnInit {
   @ViewChild("map") mapRef: ElementRef;
 
   map: google.maps.Map;
-  center: google.maps.LatLngLiteral = { lat: 30, lng: -110 };
+  center = new google.maps.LatLng(0, 0);
+  coordinates: Position;
 
   constructor() { }
 
   initMap(): void {
     this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
       center: this.center,
-      zoom: 8
+      zoom: 15
     });
   }
 
   ngOnInit(): void {
-      this.initMap();
+    this.initMap();
+    this.getCurrentPosition();
+  }
+
+  async getCurrentPosition() {
+    this.coordinates = await Geolocation.getCurrentPosition();
+    this.goPosition();
+  }
+
+  goPosition() {
+    this.center = new google.maps.LatLng(this.coordinates.coords.latitude, this.coordinates.coords.longitude);
+    this.map.setCenter(this.center);
+    this.map.setZoom(18);
+
+    new google.maps.Marker({
+      position: this.center,
+      map: this.map,
+      title: "Hello World!",
+      animation: google.maps.Animation.DROP
+    });
   }
 
 }
